0411a0514c601dcceae64fd5abcc09cf0d6165a6
Your Name <harikamurali49@gmail.com>
2026-05-14 09:14:15 +0530

    Initial Project Upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Online Quiz System 
+Technologies Used: HTML CSS JavaScript Java MySQL 
+Features: Login Quiz Score Database Connectivity 
+This project is an Online Quiz System developed using frontend and backend technologies. 
---
+++ backend/DBConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/quizdb",
+                "root",
+                ""
+            );
+
+            System.out.println("Connected Successfully");
+
+        } catch(Exception e) {
+
+            System.out.println(e);
+        }
+    }
+}
